99059e087584f088c9b56c2f79c02b8a6031dae8
boranx <boran.seref@gmail.com>
2017-05-29 20:39:01 +0300

    fix unnecessary tests

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ go get gopkg.in/jarcoal/httpmock.v1
 go get github.com/sbstjn/hanu
 
 echo "format - test - build"
-dos2unix ** && go test -v && go fmt && go build .
+go test -v && go fmt && go build .
